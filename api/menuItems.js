5e6847ad9ece66483edb076c8f55fc8f9c3c1517
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 21:18:08 +0000

    Add menuItem validation

--- api/menuItems.js
+++ api/menuItems.js
@@ -3,6 +3,15 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 const menuItemsRouter = express.Router({ mergeParams: true })
 
+// Validate a new menu items
+const validateMenuItem = (req, res, next) => {
+  const menuItem = req.body.menuItem
+  if (!menuItem.name || !menuItem.inventory || !menuItem.price || !req.params.menuId) {
+    res.sendStatus(400)
+  } else {
+    next()
+  }
+}
 // GET all saved menu items
 menuItemsRouter.get('/', (req, res, next) => {
   const sql = 'SELECT * FROM MenuItem WHERE menu_id = $menuId;'
