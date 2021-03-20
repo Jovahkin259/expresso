354a37469421731fdcdecd0625b888c57c6edac3
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 16:45:25 +0000

    add employeeId param & GET emplyee by id

--- api/employees.js
+++ api/employees.js
@@ -3,6 +3,30 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 const employeeRouter = express.Router()
 
+// Validate a new employee
+const validateEmployee = (req, res, next) => {
+  const employee = req.body.employee
+
+  if (!employee.name || !employee.position || !employee.wage) {
+    return res.sendStatus(404)
+  } else {
+    next()
+  }
+}
+
+// Check employee id parameters
+employeeRouter.param('employeeId', (req, res, next, employeeId) => {
+  db.get(`SELECT * FROM Employee WHERE id = ${employeeId}`, (error, employee) => {
+    if (error) {
+      next(error)
+    } else if (employee) {
+      req.employee = employee
+      next()
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
 // Get all employees
 employeeRouter.get('/', (req, res, next) => {
   const sql = 'SELECT * FROM Employee WHERE is_current_employee = 1;'
@@ -16,4 +40,10 @@ employeeRouter.get('/', (req, res, next) => {
     }
   })
 })
+
+// Get an employee by ID
+employeeRouter.get('/:employeeId', (req, res, next) => {
+  res.status(200).json({ employee: req.employee })
+})
+
 module.exports = employeeRouter
