d2cea24f96f482542e272885037caf56aa993130
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 15:33:36 +0000

    Added necessary middleware

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const errorHandler = require('error-handler')
 const PORT = process.env.PORT || 4000
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 app.listen(PORT, () => console.log('Server is listening on PORT: ' + PORT))
+
+app.use(errorHandler())
 module.exports = app
