7650137f12715e7dc9bbccf27f7aa3e444e867ef
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 19:40:48 +0000

    add new menu validation

--- api/menu.js
+++ api/menu.js
@@ -3,6 +3,14 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 const menuRouter = express.Router()
 
+// Validate a new menu
+const validateMenu = (req, res, next) => {
+  if (!req.body.menu.title) {
+    res.sendStatus(400)
+  } else {
+    next()
+  }
+}
 // Return all menus
 menuRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Menu', (error, menus) => {
