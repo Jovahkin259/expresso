91ed9305130a40cfbf0db0046694d5f50df7556b
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 15:53:05 +0000

    FIX api mounting route

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express()
 const sqlite3 = require('sqlite3')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const errorHandler = require('error-handler')
+const errorHandler = require('errorhandler')
 const PORT = process.env.PORT || 4000
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
+const apiRouter = require('./api/api')
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.use('/api', apiRouter)
 
 app.listen(PORT, () => console.log('Server is listening on PORT: ' + PORT))
 
