dc5c2437ff7a009d0623e52a1264efbd912518f1
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 15:16:50 +0000

    add employeeRouter and mount to apiRouter

--- api/api.js
+++ api/api.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const apiRouter = express.Router()
+const employeeRouter = require('./employee')
 
+apiRouter.use('/employees', employeeRouter)
 module.exports = apiRouter
