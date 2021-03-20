9cb77a6bfb42d60eda3dc2f7de4e6303e2c342c8
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 19:32:47 +0000

    create menu router and mount to apiRouter

--- api/api.js
+++ api/api.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const apiRouter = express.Router()
 const employeeRouter = require('./employees')
+const menuRouter = require('./menu')
 
 apiRouter.use('/employees', employeeRouter)
+apiRouter.use('/menu', menuRouter)
 module.exports = apiRouter
