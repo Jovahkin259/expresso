de8862bb4d8de1e61edf9ffecbd2209cd564a6ed
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 14:59:31 +0000

    add create Menu table

--- migration.js
+++ migration.js
@@ -4,21 +4,23 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 db.serialize(() => {
   // Create Employee table
   db.run('CREATE TABLE IF NOT EXISTS Employee (' +
-       'id INTEGER PRIMARY KEY NOT NULL, ' +
-       'name TEXT NOT NULL, ' +
-       'position TEXT NOT NULL, ' +
-       'wage INTEGER NOT NULL, ' +
-       'is_current_employee INTEGER DEFAULT 1);')
+        'id INTEGER PRIMARY KEY NOT NULL, ' +
+        'name TEXT NOT NULL, ' +
+        'position TEXT NOT NULL, ' +
+        'wage INTEGER NOT NULL, ' +
+        'is_current_employee INTEGER DEFAULT 1);')
 
   // Create Timesheet table
   db.run('CREATE TABLE IF NOT EXISTS Timesheet (' +
-           'id INTEGER PRIMARY KEY NOT NULL, ' +
-           'hours INTEGER NOT NULL, ' +
-           'rate INTEGER NOT NULL, ' +
-           'date INTEGER NOT NULL, ' +
-           'employee_id INTEGER NOT NULL, ' +
-           'FOREIGN KEY (employee_id) REFERENCES Employee(id));')
+            'id INTEGER PRIMARY KEY NOT NULL, ' +
+            'hours INTEGER NOT NULL, ' +
+            'rate INTEGER NOT NULL, ' +
+            'date INTEGER NOT NULL, ' +
+            'employee_id INTEGER NOT NULL, ' +
+            'FOREIGN KEY (employee_id) REFERENCES Employee(id));')
   // Create Menu table
-
+  db.run('CREATE TABLE IF NOT EXISTS Menu (' +
+        'id INTEGER PRIMARY KEY NOT NULL, ' +
+        'title TEXT NOT NULL)')
   // Create Menu Item table
 })
