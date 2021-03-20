fd1abca36c7c6cf34e1fc84731504d7b6a37983d
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 14:05:20 +0000

    import database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 const PORT = process.env.PORT || 4000
 
 module.exports = app
