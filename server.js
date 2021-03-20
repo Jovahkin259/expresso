6966bc6ad218da600dc56bd9eb8313f43d652d00
Joshua Astle <jovahkin259@gmail.com>
2021-03-20 14:09:22 +0000

    add server listen on PORT

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 const PORT = process.env.PORT || 4000
 
+app.listen(PORT, () => console.log('Server is listening on PORT: ' + PORT))
 module.exports = app
